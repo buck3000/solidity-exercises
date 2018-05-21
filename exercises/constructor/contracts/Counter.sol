daec2af8c7c58399ad658cec427d784003a6fdeb
Howard O'Leary <howardeoleary@gmail.com>
2018-05-21 12:01:43 -0500

    Last two exercises

--- exercises/constructor/contracts/Counter.sol
+++ exercises/constructor/contracts/Counter.sol
@@ -1,7 +1,9 @@
 pragma solidity 0.4.23;
 
 contract Counter {
-    // declare public count variable (uint)
-
-    // create a constructor wich accepts initial value of the count variable
+   uint public count;
+   
+   constructor(uint _count) public{
+     count = _count;
+   }
 }
