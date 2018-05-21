6896f0128c6f52201c82c67a1d7de1818f5ffd98
Howard O'Leary <howardeoleary@gmail.com>
2018-05-21 14:33:58 -0500

    payable exercise complete

--- exercises/payable/contracts/Balances.sol
+++ exercises/payable/contracts/Balances.sol
@@ -2,7 +2,11 @@ pragma solidity 0.4.23;
 
 
 contract Balances {
-    mapping(address => uint) public balances;
+  mapping(address => uint) public balances;
 
     // implement deposit() function and track users balances, msg.value has to be greater than 1 finney
+  function deposit() payable public{
+    require(msg.value > 1);
+    balances[msg.sender] += msg.value;
+  }
 }
